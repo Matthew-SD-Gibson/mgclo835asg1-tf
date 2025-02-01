51580349168b0bac55455084ae51fd7f452ee083
Matthew-SD-Gibson <mattsdgibson@hotmail.com>
2025-02-01 12:27:20 -0500

    Update output.tf

--- tfcode/output.tf
+++ tfcode/output.tf
@@ -1,7 +1,7 @@
 output "ecr_repository_url" {
-  value = aws_ecr_repository.my_repository.repository_url
+  value = aws_ecr_repository.mg13rep.repository_url
 }
 
 output "ec2_instance_public_ip" {
-  value = aws_instance.my_instance.public_ip
+  value = aws_instance.ec2.public_ip
 }

9e2a03d55c803aa9a8f95957262f089e20bbca7c
Matthew-SD-Gibson <mattsdgibson@hotmail.com>
2025-02-01 12:26:58 -0500

    Update main.tf

--- tfcode/main.tf
+++ tfcode/main.tf
@@ -18,6 +18,55 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+resource "aws_ecr_repository" "mg13rep" {
+  name = "${var.prefix}-repository"
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name               = "${var.prefix}-ec2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "${var.prefix}-ecr-policy"
+  description = "Allow EC2 to interact with ECR"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetRepositoryPolicy",
+          "ecr:ListImages",
+          "ecr:BatchGetImage",
+          "ecr:PutImage"
+        ]
+        Resource = "${aws_ecr_repository.mg13rep.arn}"
+        Effect   = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
+
+
 # Retrieve the default VPC
 data "aws_vpc" "default" {
   default = true
@@ -53,10 +102,11 @@ resource "aws_security_group" "ec2_sg" {
 
 
 # EC2 Instance in a Public Subnet
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2" {
   ami           = data.aws_ami.latest_amazon_linux.id
   instance_type = "t2.micro"
   key_name      = aws_key_pair.my_key.key_name
+  iam_instance_profile = aws_iam_role.ec2_role.name
   security_groups = [aws_security_group.ec2_sg.name]
 
   tags = {
